4ed1c0f9de33071819e11e1e3c4fdd9b4d1a585d
aliceDuhem <lili.duhem@gmail.com>
2021-01-18 22:36:33 +0000

    Test code that writes in a csv all the required data

---
+++ LaserControl/TestCode.py
@@ -0,0 +1,57 @@
+# This code file is used to test control codes in the laboratories
+
+import datetime
+import os
+#Write the time at which it started and the intervals of the readings
+
+#Create a class to write in the csv file all the data
+#The columns will give the intensity and the error
+def createCsvFileData(data):
+
+    docName = 'intensity_readings_'
+    date_time = datetime.datetime.now().strftime("%Y-%b-%d_%H-%M-%S")
+    file_extension =  '.csv'
+    alreadyExists_str = 'second_'
+    fileName = docName+date_time+file_extension
+    columns = ['timestamp','intensity','error']
+    separator = ','
+    format_columns = ['%s','%.6f','%.6f']
+
+    #makes sure that the headings and the columns are of the same dimensions
+    if len(data[0]) != len(columns):
+        print ("The dimensions of the columns and headings are not equal")
+        return
+
+    #check if the file exists if needed in order not to overwrite
+    if(os.path.exists(fileName)):
+        print("The file already exists")
+        fileName=docName+alreadyExists_str+date_time+file_extension
+
+    #Specifies the path mode as writing 'w'
+    file = open(fileName, 'w')
+
+    #Writes an header/description of the document: time the measures started
+    line = 'Intensity recordings for experiment starting at '+ str(data[0][0]) +'. readings every '+ str(data[1][0]-data[0][0]) + ' seconds\n'
+    file.write(line)
+
+    #each case of columns is separates on a line by a comma (separator),
+    #concatenated and added to the file
+    #line is a template for the format
+    line = separator.join(columns)
+    file.write('%s\n' % line)
+
+    #for each new row in the data, give the correct type format
+    format = separator.join(format_columns)
+    for row in data:
+        line= format % tuple(row)
+        file.write('%s\n' % line)
+
+    #close the file
+    file.close()
+
+def formatDateTime(date_time):
+    date_time = date_time.strftime("%Y-%b-%d %H:%M:%S")
+
+
+data = [[datetime.datetime.now(),0.012345,0.000001],[datetime.datetime(2021,1,19),0.234567,0.000222]]
+createCsvFileData(data)

f965e2894ed6dbdd8251f4bb5a7342f4c2d47ae7
mgr1018 <mg1018@ic.ac.uk>
2021-02-15 15:09:42 +0000

    PID

--- LaserControl/PID_loop.py
+++ LaserControl/PID_loop.py
@@ -1,44 +1,41 @@
-## this code would run in the raspberry pi console since it uses libraries specific to RPI
-## I need to check how importing libraries works with serial communication between PI and the other codes
 
 from time import sleep
 from gpiozero import Motor
 import App.Py
-import threading
 import dataGenerationScript.py
+import GetPower.py
 
 #Here we would add the function that calibrates the plate to have initial angle of 0
+def PID(KP,KD,KI,oriDictionary,SAMPLETIME):
 
-SAMPLETIME = 1 #need to find an actual value for this
+    #SAMPLETIME = 1 #need to find an actual value for this
+    #KP = 0.02
+    #KD = 0.01
+    #KI = 0.005
 
-motor = Motor(f=3, b=14) #Pin numbers I found as an example on the internet, will need to verify
+    motor = Motor(f=3, b=14)  # Pin numbers I found as an example on the internet, will need to verify
 
-# For the moment these are random coefficients
-KP = 0.02
-KD = 0.01
-KI = 0.005
+    Target_power = float_output # from App.Py
+    #power1_vector = dataGeneration(argument=1,laser_intensity=Target_power,increment=1,fileArray=1) #Not sure how to use this function...
+    # Comes from the computer
+    power1_prev_error = 0 #if we are already using the PID function in a loop in the main script, we should initiate these in the main and not in the function
+    power1_sum_error = 0
+    angle = 0
+    step_angle = 1.8  # angle turned by the motor in one step: need to find out the real value
 
-Target_power = float_output # from App.Py
-power1_vector = dataGeneration(argument=1,laser_intensity=Target_power,increment=1,fileArray=1) #Not sure how to use this function...
-power
-# Comes from the computer
-power1_prev_error = 0
-power1_sum_error = 0
-angle = 0 # will get this from Wings code I guess
-step_angle = 1.8  # angle turned by the motor in one step: need to find out the real value
+    while True:
+        power1 = readPower()
+        power1_error = Target_power - power1.value
+        power1 += (power1_error * KP) + (power1_prev_error * KD) + (power1_sum_error * KI)
+        Pd = Pc_to_Pd(angle, cube_transmittance = 1, cube_ref_trans = 1)
+        dtheta = neededAngle(angle,Pd, power1, oriDictionary)
+        step_nb = dtheta // step_angle  # number of steps the motor will make to turn to the desired angled
+        for i in range(step_nb):
+            motor.forward()
+        power1.reset()
+        sleep(SAMPLETIME)
+        power1_prev_error = power1_error
+        power1_sum_error += power1_error
+        angle = step_nb * step_angle
 
-while True:
-    power1_error = Target_power - power1.value
-    power1 += (power1_error * KP) + (power1_prev_error * KD) + (power1_sum_error * KI)
-    dtheta = find_angle(power1,angle) ##find_angle would be Wings function...I can always change this part
-    step_nb = dtheta // step_angle  # number of steps the motor will make to turn to the desired angled
-    for i in range(step_nb):
-        motor.forward()
-    power1.reset()
-    sleep(SAMPLETIME)
-    power1_prev_error = power1_error
-    power1_sum_error += power1_error
-    angle = dtheta #Not sure this is true, is dtheta the optimal angle or the closest possible angle to the optimal one?
-
-#Here we would add the function that resets the angle to 0 at the end
 

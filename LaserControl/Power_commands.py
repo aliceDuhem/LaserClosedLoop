011074b11648026260838d4d2f4265e26411fd22
aliceDuhem <lili.duhem@gmail.com>
2021-02-26 15:14:22 +0000

    Tested with power meter and it works (deleted timeout)

--- LaserControl/Power_commands.py
+++ LaserControl/Power_commands.py
@@ -1,19 +1,21 @@
 # Install : python setup.py install
 
 #Creates the createInstrumentimport visa
+import pyvisa as visa
 from ThorlabsPM100 import ThorlabsPM100
+
 rm = visa.ResourceManager()
-inst = rm.open_resource('USB0::0x0000::0x0000::xxxxxxxxx::INSTR',
-                        term_chars='\n', timeout=1)
+inst = rm.open_resource('USB0::0x0000::0x0000::xxxxxxxx::INSTR')
 power_meter = ThorlabsPM100(inst=inst)
 
 #Alternative, use usbtmc
-from ThorlabsPM100 import ThorlabsPM100, USBTMC
-inst = USBTMC(device="/dev/usbtmc0")
-power_meter = ThorlabsPM100(inst=inst)
+#from ThorlabsPM100 import ThorlabsPM100, USBTMC
+#inst = USBTMC(device="/dev/usbtmc0")
+#power_meter = ThorlabsPM100(inst=inst)
 
 #All the commands
-print power_meter.read # Read-only property
-print power_meter.sense.average.count # read property
-power_meter.sense.average.count = 10 # write property
-power_meter.system.beeper.immediate() # method
+print (power_meter.read) # Read-only property
+
+#print power_meter.sense.average.count # read property
+#power_meter.sense.average.count = 10 # write property
+#power_meter.system.beeper.immediate() # method

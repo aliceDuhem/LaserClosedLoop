20d8723fd6905c9140835b7e2b008e3470d184ac
aliceDuhem <lili.duhem@gmail.com>
2021-06-21 13:51:22 +0200

    Added Pc from Pd function

--- LaserControl/RatioFinal.py
+++ LaserControl/RatioFinal.py
@@ -3,7 +3,7 @@ import sys
 
 
 class ratio:
-    def Plaser_from_Pd(Pd, motor_angle, cube_ref_trans=1, halfWave_transmittance =0.95):
+    def Plaser_from_Pd(Pd, motor_angle, cube_ref_trans=0.95, halfWave_transmittance =0.9):
 
         #Convert angles to 0<angle<360
         if motor_angle>360:
@@ -15,6 +15,21 @@ class ratio:
         #ratio = 4* cube_ref_trans * halfWave_transmittance * pow(math.cos(motor_angle),2) * pow(math.sin(motor_angle),2)
         ratio = 0.5*cube_ref_trans * halfWave_transmittance * math.sin(4*motor_angle-math.pi/2) + (halfWave_transmittance)/2
         return Pd*1/ratio
+
+    def Pc_from_Pd(Pd, motor_angle, cube_ref_trans=0.95,cube_trans=0.9, halfWave_transmittance =0.9):
+        #Convert angles to 0<angle<360
+        if motor_angle>360:
+            motor_angle = motor_angle%360
+
+        #trig in math module works in RAD, convert motor increenet in degrees to rad
+        motor_angle = math.radians(motor_angle)
+
+        #First find Plaser
+        Po=Plaser_from_Pd(Pd, motor_angle, cube_ref_trans, halfWave_transmittance)
+
+        #ratio = 4* cube_ref_trans * halfWave_transmittance * pow(math.cos(motor_angle),2) * pow(math.sin(motor_angle),2)
+        Pc= 0.5*Po*(halfWave_transmittance*cube_trans* math.sin(4*motor_angle+math.pi/2) + (halfWave_transmittance))
+        return Pc
 
 
 #class with functions which output absolute values, eg. Pc Pd
